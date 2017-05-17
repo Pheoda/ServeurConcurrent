3f1d263c935905ebc17ca116895fb3db688e3cad
Alexis Fougerouse <alexis.fougerouse2@etu.univ-lyon1.fr>
2017-05-17 19:18:41 +0200

    Update TP2.java

--- src/tp2/TP2.java
+++ src/tp2/TP2.java
@@ -6,31 +6,31 @@ import java.net.InetAddress;
 public class TP2 {
 
     public static void main(String[] args) throws IOException {
-        Serveur s = new Serveur();
+       /* Serveur s = new Serveur();
         new Thread(s).start();
         
         
         Client client1 = new Client();
         new Thread(client1).start();
-        /*lient1.connecter(InetAddress.getByName("127.0.0.1"), s.getPort());
-        client1.envoyer("Message test client 1");*/
+        Client1.connecter(InetAddress.getByName("127.0.0.1"), s.getPort());
+        client1.envoyer("Message test client 1");
 
         Client client2 = new Client();
         new Thread(client2).start();
 
-       /* client2.connecter(InetAddress.getByName("127.0.0.1"), s.getPort());
-        client2.envoyer("Message test client 2");*/
+        client2.connecter(InetAddress.getByName("127.0.0.1"), s.getPort());
+        client2.envoyer("Message test client 2");
         
-        //Client client3 = new Client();
-        /*client3.connecter(InetAddress.getByName("127.0.0.1"), s.getPort());
+        Client client3 = new Client();
+        client3.connecter(InetAddress.getByName("127.0.0.1"), s.getPort());
         client3.envoyer("Message test client 3 au serveur");
-*/
+
         
-       /* client2.envoyer("Autre test client 2");
+        client2.envoyer("Autre test client 2");
         client1.envoyer("Autre test client 1");
-        client3.envoyer("Autre test client 3");*/
+        client3.envoyer("Autre test client 3");
         
-       /* client1.fermer();
+        client1.fermer();
         client2.fermer();
         client3.fermer();*/
     }
